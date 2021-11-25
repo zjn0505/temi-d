2926d7c1b26f1a5a3e10f895edea58ee68474a29
PaulTemi <paul.song@robotemi.com>
2021-11-25 17:52:59 +0800

    add frontend

--- pages/web/hotel/[hotelId].js
+++ pages/web/hotel/[hotelId].js
@@ -1,61 +1,92 @@
 import database from '../../../firebase/clientApp';
 import {
     ref,
-    onValue
+    onValue,
 } from "firebase/database";
+import { useState } from 'react';
 
-import { useRouter } from 'next/router'
-
-function getHotelData(hotelId, callback) {
-    return onValue(ref(database, '/hotel/' + hotelId), (snapshot) => {
-        const hotel = snapshot.val();
-        callback(hotel)
-    }, {
-        onlyOnce: true
-    });
-}
-
-const RegisterPage = () => {
-    const router = useRouter()
-    const { hotelId } = router.query
-
-    getHotelData(hotelId, (hotel) => {
-        // res.status(200).json(hotel)
-        // TODO return html
-        console.log(`${JSON.stringify(hotel)}`)
-        return <p> Post: { JSON.stringify(hotel) } </p>
+function getHotelData(hotelId) {
+    return new Promise((res, rej) => {
+        onValue(ref(database, '/hotel/' + hotelId), (snapshot) => {
+            const hotel = snapshot.val();
+            res(hotel)
+        }, {
+            onlyOnce: true
+        });
     })
-
-    console.log(hotelId)
-    // return <p> Post: { hotelId } </p>
 }
-
-
-export async function getServerSideProps({ params }) {
-    console.log(`https://.../posts/${params.hotelId}`)
-    const res = await fetch(`https://.../posts/${params.id}`)
-    const post = await res.json()
-  
-    // Pass post data to the page via props
-    return { props: { post } }
+function submitForm (obj){
+    fetch('/api/web/register', {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {
+            'content-type': 'application/json',
+        }
+    })
+}
+export async function getServerSideProps({params}) {
+    const { hotelId } = params
+    const hotel = await getHotelData(hotelId)
+    return {
+      props: {
+        hotel,
+        hotelId,
+      },
+    }
   }
 
-export default RegisterPage
+  export default function RegisterPage ({hotel, hotelId}) {
+    const [floor, setFloor] = useState(null)
+    const [location, setLocation] = useState('')
+    const [userName, setUserName] = useState('')
+    const [tel, setTel] = useState('')
 
-// export default function handler(req, res) {
-//     if (req.method === 'GET') {
-//         // console.log("req =" + JSON.stringify(req.body))
-//         let { hotelId } = req.query
-//         if (hotelId) {
-//             console.log(`hotel ${hotelId}`)
-            
-//         } else {
-//             res.status(400).json({
-//                 error: "invalid param"
-//             })
-//         }
-
-//     } else {
-//         res.status(405).send()
-//     }
-// }
+    const floorList = hotel.locations.map(({floor}) => {
+        return <option key={floor} value={floor}>{floor}层</option>
+    })
+    const locations = () => {
+        const currFloor = hotel.locations.find(item => item.floor === floor)
+        if(currFloor){
+            return currFloor.locations.filter(loc => !['elevator', 'elevator door'].includes(loc)).map((loc, index) => {
+                return <option key={index + loc} value={loc}>{loc}</option>
+            })
+        } else return []
+    }
+    const submitHandler = () => {
+        const payload = {
+            "hotelId": hotelId,
+            "user": {
+                "name": userName,
+                "phone": tel,
+                "floor": floor,
+                "location": location
+            }
+        }
+        submitForm(payload)
+    }
+    return (
+        <div className="row justify-content-center">
+            <div className="col-12 col-sm-10 col-lg-6">
+                <title>Register</title>
+                <h1>Register</h1>
+                <div className="input-group mb-3 mt-3">
+                    <span className="input-group-text" id="basic-addon1">姓名</span>
+                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" onChange={(e) => setUserName(e.target.value)}/>
+                </div>
+                <div className="input-group mb-3 mt-3">
+                    <span className="input-group-text" id="basic-addon1">手机</span>
+                    <input type="tel" className="form-control" placeholder="Phone" aria-label="Phone" aria-describedby="basic-addon1" onChange={(e) => setTel(e.target.value)}/>
+                </div>
+                <select className="form-select mb-3 mt-3" defaultValue="null" aria-label="select floor" onChange={(e) => setFloor(e.target.value)}>
+                    <option value="null" disabled>楼层</option>
+                    {floorList}
+                </select>
+                <select className="form-select mb-3 mt-3" defaultValue="null" aria-label="select location" onChange={(e) => setLocation(e.target.value)}>
+                    <option value="null" disabled>地点</option>
+                    {locations()}
+                </select>
+                <button className="btn btn-primary" onClick={submitHandler}>提交</button>
+            </div>
+        </div>
+    )
+}
